f7afd4b25dcbc77478969843d68e0f30b94a8d40
BoomWattanachai <boom_sirisuk@hotmail.com>
2020-04-14 15:47:43 +0700

    Filter patient base on current user(doctor)

--- hospital/models/doctor.py
+++ hospital/models/doctor.py
@@ -12,3 +12,4 @@ class Doctor(models.Model):
     gender = fields.Selection((('male','Male'),('female','Female')),required=True)
     # 1 patient 1 doctor / 1 doctor many patient
     patient_name = fields.One2many('hospital.patient', 'doctor_name', ondelete='set null', string="Sessions")
+    user_ref = fields.Many2one('res.users', ondelete='set null', string="User", index=True,required=True)
